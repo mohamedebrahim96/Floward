022871cfb03f8d6e7ed9460c1fde76c4f1f6c639
Mohamed Ebrahim <ebrahimm131@gmail.com>
2022-09-18 17:03:29 +0400

    add app class and details activity

---
+++ app/src/main/java/com/floward/FlowardApp.kt
@@ -0,0 +1,4 @@
+package com.floward
+
+class FlowardApp {
+}
--- app/src/main/java/com/floward/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/floward/ui/details/DetailsActivity.kt
@@ -1,9 +1,10 @@
-package com.floward
+package com.floward.ui.details
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.floward.R
 
-class MainActivity : AppCompatActivity() {
+class DetailsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
